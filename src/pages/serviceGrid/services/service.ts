2d1290f0a68ff1f3511ea81f3c88c823a90b69a0
SKYkubeedge <kubeedge@orientsky.com.cn>
2023-01-09 17:17:00 +0800

    initial kubeedge UI dashboard
    Signed-off-by: SKYkubeedge <kubeedge@orientsky.com.cn>

---
+++ src/pages/serviceGrid/services/service.ts
@@ -0,0 +1,35 @@
+import request from '@/utils/request';
+import type { DeptType } from './data.d';
+
+// 列表
+export function getList(namespaces: string) {
+ return request(`/v1/namespaces/${namespaces}/services`, {
+  method: 'get'
+ }) 
+}
+// 删除
+export function removeItem(app: string) {
+  return request(`/apis/apps.kubeedge.io/v1alpha1/nodegroups/${app}`, {
+    method: 'delete',
+  });
+}
+
+
+
+
+
+// 新增部门
+export async function addDept(params: DeptType) {
+  return request('/system/dept', {
+    method: 'POST',
+    data: params,
+  });
+}
+
+// 修改部门
+export async function updateDept(params: DeptType) {
+  return request('/system/dept', {
+    method: 'PUT',
+    data: params,
+  });
+}
