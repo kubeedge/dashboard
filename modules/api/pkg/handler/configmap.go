2db5dfb8348dd3404dd312466b094a3a1609bf51
DoisLONG <15221580643@163.com>
2025-06-03 20:05:08 +0800

    add copyright and sort import segments
    Signed-off-by: DoisLONG <15221580643@163.com>

--- modules/api/pkg/handler/configmap.go
+++ modules/api/pkg/handler/configmap.go
@@ -1,13 +1,30 @@
+/*
+Copyright 2025 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package handler
 
 import (
 	"net/http"
 
 	"github.com/emicklei/go-restful/v3"
+	corev1 "k8s.io/api/core/v1"
+
 	"github.com/kubeedge/dashboard/api/pkg/resource/configmap"
 	"github.com/kubeedge/dashboard/client"
 	"github.com/kubeedge/dashboard/errors"
-	corev1 "k8s.io/api/core/v1"
 )
 
 func (apiHandler *APIHandler) addConfigMapRoutes(apiV1Ws *restful.WebService) *APIHandler {
