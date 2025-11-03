da76fe465782a6e02eceb42c49e07da7a9413957
Chen Su <ghosind@gmail.com>
2025-11-03 18:42:03 +0800

    fix: fix namespace selector error
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- modules/web/src/component/Layout/NamespaceSelector.tsx
+++ modules/web/src/component/Layout/NamespaceSelector.tsx
@@ -1,22 +1,25 @@
 'use client';
 
 import { useListNamespaces } from '@/api/namespace';
+import { useI18n } from '@/hook/useI18n';
 import { useNamespace } from '@/hook/useNamespace';
 import { MenuItem, Select } from '@mui/material';
 
 export default function NamespaceSelector() {
   const {namespace, setNamespace} = useNamespace();
   const { data: namespaces } = useListNamespaces();
+   const { t } = useI18n();
 
   return (
     <Select
-      value={namespace}
+      value={namespace || ''}
       onChange={(e) => setNamespace(e.target.value)}
       variant="standard"
       disableUnderline
+      displayEmpty
       sx={{ color: 'white', mr: 2 }}
     >
-      <MenuItem defaultChecked value="">All NameSpace</MenuItem>
+      <MenuItem key={'all-namespace'} value={''}>All Namespaces</MenuItem>
       {namespaces?.items.map((ns) => (
         <MenuItem key={ns.metadata?.name} value={ns.metadata?.name}>
           {ns.metadata?.name}
